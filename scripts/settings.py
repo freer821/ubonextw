043dfb7dedeb1f6f9890b3e1d05df9ee26230d48
ZhenyuGeng <freergzy8212@hotmail.com>
2019-09-14 16:39:27 +0200

    add test

--- scripts/settings.py
+++ scripts/settings.py
@@ -8,6 +8,9 @@ if env == 'dev':
 elif env == 'prod':
     print('starting prod environment.....')
     from .settings_production import *
+elif env == 'test':
+    print('starting test environment.....')
+    from .settings_test import *
 else:
     print('starting dev environment.....')
     from .settings_development import *
